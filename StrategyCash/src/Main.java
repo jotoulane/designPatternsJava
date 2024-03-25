f444ebdd2c0609d266ed6077bf5b02acc36470d0
andrew <3476511424@qq.com>
2024-03-25 09:05:22 +0800

    StrategyCash

--- StrategyCash/src/Main.java
+++ StrategyCash/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
+    static double PRICE = 5d;
+    static double NUM = 10d;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double total = 0.0d;
+        CashContext csuper = new CashContext("正常收费");
+        double totalPrices = 0d;
+        totalPrices = csuper.GetResult(PRICE * NUM);
+        total+=totalPrices;
+
     }
 }
